28bdd8909c2343391bbef23f529ca2ba6eec05fc
bawfng04 <nguyendinhbang53az@gmail.com>
2025-01-22 12:38:57 +0700

    change connect type to IP_ADDRESS

--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const IP_ADDRESS = process.env.IP_ADDRESS || "localhost";
 
 const config = {
-  server: "NPM-SASS",
+  server: IP_ADDRESS || "NPM-SASS",
+
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -14,6 +15,12 @@ const config = {
     charset: "UTF-8",
   },
 };
+
+if (IP_ADDRESS) {
+  console.log("connected to database via IP address");
+} else {
+  console.log("connected to database via localhost");
+}
 
 const poolPromise = new sql.ConnectionPool(config)
   .connect()
